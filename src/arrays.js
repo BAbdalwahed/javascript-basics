104b9a2615357486eb7eb6fdd5495e6f649f3da9
BAbdalwahed <b.abdalwahed@hotmail.com>
2022-10-30 16:57:45 +0000

    arrays up to uppercaseWordsInArray

--- src/arrays.js
+++ src/arrays.js
@@ -1,61 +1,61 @@
 const getNthElement = (index, array) => {
-  // your code here
+  return array[index%array.length];
 };
 
 const arrayToCSVString = array => {
-  // your code here
+  return array.join(',');
 };
 
 const csvStringToArray = string => {
-  // your code here
+  return string.split(',');
 };
 
 const addToArray = (element, array) => {
-  // your code here
+  array.push(element);
 };
 
-const addToArray2 = (element, array) => {
-  // your code here
-};
+function addToArray2(element, array) {
+  return array.concat(element);
+}
 
 const removeNthElement = (index, array) => {
-  // your code here
+  return array.splice(index, 1);
 };
 
 const numbersToStrings = numbers => {
-  // your code here
+  return numbers.toString().split(",");
 };
 
 const uppercaseWordsInArray = strings => {
-  // your code here
+  
 };
 
 const reverseWordsInArray = strings => {
-  // your code here
+  
 };
 
 const onlyEven = numbers => {
-  // your code here
+  
 };
 
 const removeNthElement2 = (index, array) => {
-  // your code here
+  
 };
 
 const elementsStartingWithAVowel = strings => {
-  // your code here
+  
 };
 
 const removeSpaces = string => {
-  // your code here
+  
 };
 
 const sumNumbers = numbers => {
-  // your code here
+  
 };
 
 const sortByLastLetter = strings => {
-  // your code here
+  
 };
 
 module.exports = {
